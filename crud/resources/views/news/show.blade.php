6b75ddd8ac5f4db19a3f3a092a5ac8aa0f21a419
maan <pranijman@gmail.com>
2018-10-15 15:12:49 +0545

    view news ... before image

--- crud/resources/views/news/show.blade.php
+++ crud/resources/views/news/show.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Add New News</title>
+    <title>View News</title>
     <link rel="stylesheet" type="text/css" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- <script type="text/javascript" src="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> -->
@@ -51,36 +51,38 @@
     <div class="row">
         <div class="col-md-9 content">
 
-            <div style="padding: 10px 0;">Add News Form</div>
+            <div style="padding: 10px 0;">View News</div>
 
             <form method="post" action="{{route('news.store')}}" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="title" value="<?=$news->title?>">
+                    <div type="text" class="form-control" id="title" name="title" placeholder="title"><?=$news->title?></div>
                 </div>
 
                 <div class="form-group">
                     <label for="image">Image</label>
-                    <input type="file" class="form-control-file" name="image" id="image" value="**<?=$news->id?>">
+                    <input type="file" class="form-control-file" name="image" id="image" value="<?=$news->id?>">
                 </div>
 
                 <div class="form-group">
                     <label for="newsDescription">News Editor</label>
-                    <textarea class="form-control" id="newsDescription" name="newsDescription" rows="3"><?=$news->content?></textarea>
+                    <div class="form-control" id="newsDescription" name="newsDescription" rows="3" style="height: 100px;"><?=$news->content?></div>
                 </div>
 
                 <div class="form-group">
                     <label for="publishedDate">Published Date</label>
-                    <input type="text" class="form-control" id="publishedDate" name="publishedDate" placeholder="publishedDate" value="<?=$news->Published_date?>">
+                    <div type="text" class="form-control" id="publishedDate" name="publishedDate" placeholder="publishedDate"><?=$news->Published_date?></div>
                 </div>
 
                 <div class="form-group">
                     <label for="author">Author</label>
-                    <input type="text" class="form-control" id="author" name="author" placeholder="author" value="<?=$news->author?>">
+                    <div type="text" class="form-control" id="author" name="author" placeholder="author"><?=$news->author?></div>
                 </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="edit" class="btn btn-primary">Edit</button>
+                <button type="delete" class="btn btn-primary">Delete</button>
+
                 <input type="hidden" name="categoryId" id="categoryId" value="<?=$news->category_id?>">
             </form>
         </div>
@@ -96,6 +98,7 @@
                             <input type="radio" name="categories" value="<?=$category->id?>"
                                 <?=($news->category_id==$category->id)?'checked="checked"':''?>
                                 onClick="document.getElementById('categoryId').value=this.value"
+                                disabled="disabled"
                             >
                             <?=$category->Display_name?>
                         </li>
